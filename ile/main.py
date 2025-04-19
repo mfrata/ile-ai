e1dcd57721b82f5e2c0929caf77ec7846794d61b
Matheus Frata <git@mfrata.com>
2025-04-19 15:05:08 +0200

    feat: add entrypoint

--- ile/main.py
+++ ile/main.py
@@ -9,7 +9,7 @@ app = typer.Typer()
 
 
 @app.command()
-def extract(file: Path):
+def extract(file: Path = typer.Argument(..., help="The path to the file to extract transactions from")):
     print(extract_transactions(file.read_text()))
 
 
