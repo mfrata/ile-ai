600b6a527d63f1d1d058fbce7d94bfb81316dcb0
Matheus Frata <git@mfrata.com>
2025-04-19 13:55:16 +0200

    feat: add simple transaction parser

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ile-ai!")
+import sys
+from openai import OpenAI
 
+client = OpenAI()
 
-if __name__ == "__main__":
-    main()
+transactions_text = open(sys.argv[1]).read()
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "You are a extractor of transactions from a file"},
+        {"role": "user", "content": f"Return the list of transactions from this text: \n {transactions_text}"},
+    ],
+)
+
+print(response.choices[0].message.content)
